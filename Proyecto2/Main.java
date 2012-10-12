4161ab8a9db66ce49bb86b1fad33738e632b980c
glimonta <glimonta@gmail.com>
2012-10-12 11:12:26 -0430

    Cola linda y bella totalmente funcional solo probada con enteros

---
+++ Proyecto2/Main.java
@@ -0,0 +1,82 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cola colita = new Cola(20);
+		
+		System.out.println("Veremos que la nueva cola que creamos es vacia: ");
+		System.out.println(colita.esVacia());
+		
+		System.out.println("Encolaremos 21 elementos a la cola (el max es 20):");
+		System.out.println(colita.encolar(1));
+		System.out.println(colita.encolar(2));
+		System.out.println(colita.encolar(3));
+		System.out.println(colita.encolar(4));
+		System.out.println(colita.encolar(5));
+		System.out.println(colita.encolar(6));
+		System.out.println(colita.encolar(7));
+		System.out.println(colita.encolar(8));
+		System.out.println(colita.encolar(9));
+		System.out.println(colita.encolar(10));
+		System.out.println(colita.encolar(11));
+		System.out.println(colita.encolar(12));
+		System.out.println(colita.encolar(13));
+		System.out.println(colita.encolar(14));
+		System.out.println(colita.encolar(15));
+		System.out.println(colita.encolar(16));
+		System.out.println(colita.encolar(17));
+		System.out.println(colita.encolar(18));
+		System.out.println(colita.encolar(19));
+		System.out.println(colita.encolar(20));
+		System.out.println(colita.encolar(21));
+		
+		System.out.println("Veremos si la cola es vacia: ");
+		System.out.println(colita.esVacia());
+		
+		System.out.println("Haremos primero y luego desencolaremos 21 veces (solo hay 20 elementos):");
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		System.out.println(colita.primero());
+		System.out.println(colita.desencolar());
+		
+	}
+
+}
