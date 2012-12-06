62cd44f67fc6b07cb5686dbe8af6acb33666585f
glimonta <glimonta@gmail.com>
2012-12-06 01:47:44 -0430

    Main comentado

--- Proyecto 5/Main.java
+++ Proyecto 5/Main.java
@@ -1,3 +1,10 @@
+/**
+ * 
+ * @author Gabriela Limonta, Luis Miranda
+ * 
+ * Clase que contiene el main del programa.
+ */
+
 import java.io.*;
 import java.util.Scanner;
 
@@ -5,12 +12,20 @@ public class Main {
 	
 	static Scanner scan = null;
 	
+	/*
+	 * leerArchivo: dado el numero de escenarios se encarga de leer el archivo
+	 * para buscar las ciudades y las conexiones entre ellas que deben 
+	 * establecerse para formar nuestro grafo.
+	 */
 	public static int leerArchivo(int numEscenario) {
         
 		DynamicArray lados = new DynamicArray(2);
+		//leemos el numero de ciudades del escenario
 		int numCiudad = scan.nextInt();
-		//System.out.println(numCiudad);
+		//en este ciclo agregaremos los lados correspondientes a cada ciudad
 		for(int i=0;i<numCiudad;i++) {
+			// este trozo de codigo es para ignorar el string que leemos ya que
+			// no nos importa
 			String bleh = scan.nextLine();
 			char[] arrChar = bleh.toCharArray();
 
@@ -18,73 +33,76 @@ public class Main {
 	            bleh = scan.nextLine();
 	            arrChar = bleh.toCharArray();
 	        }
-	        //System.out.println(bleh);
 			int numVecinas = scan.nextInt();
-			//System.out.println(numVecinas);
+			/* numVecinas es el numero de ciudades con las que se conecta y
+			 * para todas las vecinas leemos quien es y el costo y creamos 
+			 * un nuevo arco que agregamos a la lista de arcos.
+			 */
 			for(int k=0;k<numVecinas;k++) {
 				int vec = scan.nextInt();
 				int costo = scan.nextInt();
-				//System.out.println(vec+" "+costo);
 				Arco aux = new Arco(i+1,vec,costo);
 				lados.addFinal(aux);
 			}
 		}
+		/* luego vienen las ciudades que ya estan conectadas,
+		 * aca leemos que ciudad se conecta con quien (c1 y c2) y las
+		 * agregamos a la lista de lados con costo cero ya que al estar
+		 * conectadas ya el problema de su costo fue resuelto.
+		 */
 		int yaConect = scan.nextInt();
-		//System.out.println(yaConect);
 		for(int j=0;j<yaConect;j++) {
 			int c1 = scan.nextInt();
 			int c2 = scan.nextInt();
 			Arco aux2 = new Arco(c1,c2,0);
 			lados.addFinal(aux2);
 		}
-		int lados2 = Kruskal(lados,numCiudad);
-		return lados2;
+		/* ahora aplicamos kruskal a los lados que leimos del archivo y
+		 * kruskal nos devuelve el costo del minimo arbol cobertor.  
+		 */
+		int cost = Kruskal(lados,numCiudad);
+		//devolvemos eso
+		return cost;
 	}
 	
+	/*
+	 * Kruskal: es la implementacion del algoritmo de Kruskal con la unica 
+	 * diferencia de que devuelve el costo del arbol cobertor de costo minimo.
+	 * toma una lista de lados y un numero de ciudades.
+	 */
 	public static int Kruskal(DynamicArray lad, int nC) {
 		DisjointSet E = new DisjointSet(nC);
 		DynamicArray ladK = new DynamicArray(2);
-/*		
-		for(int j=0;j<lad.getPosicion();j++){
-			System.out.println(lad.get(j).toString());
-		}
-*/		
-//		System.out.println("\n-----------\n");
-		
+
+		//ordenamos los lados de menor a mayor
 		lad.heapSort();
-/*		
-		for(int j=0;j<lad.getPosicion();j++){
-			System.out.println(lad.get(j).toString());
-		}
-*/		
 		
 		int i=0;
 		int cost=0;
+		/* mientras aun haya mas de 1 componente conexa es porque no 
+		 * hemos logrado conectar todos los nodos mediante arcos 
+		 */
 		while (E.getConexas() > 1) {
-//			System.out.println("\n--- Iteracion "+i+"---\n");
 			Arco e = (Arco) lad.get(i);
-//			System.out.println("\n--- Revisamos el arco: "+e.toString()+"---\n ");
-			int[] rep = E.getRepres();
-//			System.out.println("\n--- origen: "+rep[e.getSrc()]+" ---\n");
-//			System.out.println("\n--- destino: "+rep[e.getDst()]+" ---\n");
+			//si pertenecen a componentes conexas dif, las uno y sumo el costo
+			//de pasar por este lado
 			if (E.find(e.getSrc()) != E.find(e.getDst())) {
-//				System.out.println("\n---Entre al if---\n");
 				E.union(e.getSrc(), e.getDst());
-				//ladK.addFinal(e);
 				cost += e.getCost();
 			}
 			i++;
 		}
+		//devuelvo el costo
 		return cost;
 	}
 	
+	/*
+	 * imprimirArch: a esta funcion se le pasa un entero (lo que vamos a
+	 * imprimir en el archivo) y un BufferedWriter y lo que hace es imprimir
+	 * el entero en el archivo de salida.
+	 */
 	public static void imprimirArch(int lK, BufferedWriter out) {
 		try {
-			//int cost = 0;
-			//for(int k=0;k<lK.getPosicion();k++) {
-				//Arco act = (Arco) lK.get(k);
-				//cost += act.getCost();
-			//}
 			out.write(lK+"\n");
 			System.out.println(lK);
 		} catch (IOException e) {
@@ -105,6 +123,7 @@ public class Main {
 	File arch = new File(in);
 	FileWriter sali = null;
 	
+	//creo lo necesario para escribir en el archivo
 	try {
 		sali = new FileWriter(new File(out));
 	}
@@ -117,6 +136,7 @@ public class Main {
 	try{
 		scan = new Scanner(arch);
 		int numEsc = scan.nextInt();
+		//para cada escenario se hace leerArchivo e imprimirArchivo
 		for(int i=0;i<numEsc;i++) {
 			int ladKrus = leerArchivo(i);
 			imprimirArch(ladKrus,outFile);
